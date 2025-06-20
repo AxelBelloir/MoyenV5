5e1e890c522287e45ce653ec40c95a74d6a076b5
AxelBelloir <axelbelloir1@gmail.com>
2025-06-20 19:21:03 +0200

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,1 +1,13 @@
+import sqlite3
+
+conn = sqlite3.connect('utilisateurs.db')
+conn.execute('''
+CREATE TABLE IF NOT EXISTS utilisateurs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT UNIQUE NOT NULL,
+    mot_de_passe TEXT NOT NULL
+)
+''')
+conn.close()
+print("Base initialisée🗃️")
 
