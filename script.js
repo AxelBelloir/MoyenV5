08352d0a9e60f59e2f7f391dcc2df212f70d8103
AxelBelloir <axelbelloir1@gmail.com>
2026-09-20 10:40:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const DIV_principal = ["overlay","DIV_home","DIV_login","DIV_average","DIV_chess"];
 
-function open(const DIV){
+function open(DIV){
   for(let i = 0; i < DIV_principal.length;++i){
     document.getElementById(DIV_principal[i]).style.display =  "none";
   }
