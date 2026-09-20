37f5d4a29aa7020c5518d41c4a5d450f871947ab
AxelBelloir <axelbelloir1@gmail.com>
2026-09-20 10:20:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-
+const DIV_principal = ["overlay","]
 
 function open(const DIV){
+  for(let i = 0; i < DIV_principal.size()){
+    
+  }
   document.getElementById(DIV).style.display = "block";
+  document.getElementById("overlay").style.display = "block";
 }
