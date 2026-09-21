ccb21810801d55e330df3d5dea78f7abd7690d2e
AxelBelloir <axelbelloir1@gmail.com>
2026-09-21 07:04:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const DIV_principal = ["overlay","DIV_home","DIV_login","DIV_average","DIV_chess"];
 
 function openDIV(DIV){
-  for(let i = 0; i < DIV_principal.length;++i){
-    document.getElementById(DIV_principal[i]).style.display =  "none";
-  }
+  if(DIV != "DIV_login"){
+    for(let i = 0; i < DIV_principal.length;++i){
+      document.getElementById(DIV_principal[i]).style.display =  "none";
+    }
+  } else { document.getElementById("overlay").style.display = "block";}
   document.getElementById(DIV).style.display = "block";
-  document.getElementById("overlay").style.display = "block";
 }
